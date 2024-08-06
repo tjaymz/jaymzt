537b4fef5996c8da9d816f26775e19dc08eb1f6f
tjaymz <tjaymz@gmail.com>
2024-08-06 08:50:50 -0400

    Reworking of the entire website

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,12 @@
+import Leaf
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.views.use(.leaf)
     // register routes
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,19 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get { req async throws in
+    try await req.view.render("home", ["title": "Home", "name": "James T"])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("about") { req async throws in
+        try await req.view.render("about", ["title": "About"])
+    }
+    
+    app.get("portfolio") { req async throws in
+        try await req.view.render("portfolio", ["title": "Portfolio"])
+    }
+    
+    app.get("testing") { req async throws in
+        try await req.view.render("testing", ["title": "Experimental & Projects"])
     }
 }
